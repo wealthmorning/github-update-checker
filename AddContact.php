cb8531291fd6a43186815f98cbb5a331d3e98c24
wealthmorning <52590189+wealthmorning@users.noreply.github.com>
2019-12-31 15:24:35 +1300

    Create AddContact.php

---
+++ AddContact.php
@@ -0,0 +1,71 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+
+    $token = 'Your Sendgrid Api Key';
+    $listId = 'Your Sendgrid List ID';
+   
+
+$headers = [
+    'Authorization' => 'Bearer '. $token,
+    'Content-Type' => 'application/json',
+    'Accept' => 'application/json',
+];
+
+$email = $_POST['email'] ?? $_GET['email'] ?? null;
+
+$data = [
+    "email" => $email,
+];
+$data_string = json_encode($data);
+
+$client = new Client();
+
+$response = $client->post('https://api.sendgrid.com/v3/contactdb/recipients', [
+    'headers' => $headers,
+    'json' => [
+        $data
+    ],
+]);
+
+if ($response->getBody()) {
+    $result = json_decode($response->getBody());
+} else {
+    echo(json_encode([
+        'message' => "Request failed",
+        'code' => 500,
+    ]));
+    die;
+}
+
+$id = $result->persisted_recipients[0] ?? null;
+
+if ($id) {
+
+    $response = $client->post("https://api.sendgrid.com/v3/contactdb/lists/$listId/recipients/$id", [
+        'headers' => $headers,
+        'json' => [
+            $data
+        ],
+    ]);
+  
+    if ($response->getBody()) {
+        $result = json_decode($response->getBody());
+    } else {
+        echo(json_encode([
+            'message' => "Request failed",
+            'code' => 500,
+        ]));
+        die;
+    }
+
+}
+
+echo(json_encode([
+    'message' => "Subscribed",
+    'code' => 200,
+]));
+die;
