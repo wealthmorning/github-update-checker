a1d6d12e6d07c997a03acf365dc912fe0767df16
wealthmorning <52590189+wealthmorning@users.noreply.github.com>
2019-12-31 15:39:19 +1300

    Update AddContact.php

--- AddContact.php
+++ AddContact.php
@@ -56,7 +56,7 @@ if ($id) {
         $result = json_decode($response->getBody());
     } else {
         echo(json_encode([
-            'message' => "Request failed",
+            'message' => "Failed to Add Contact to List",
             'code' => 500,
         ]));
         die;
@@ -65,7 +65,7 @@ if ($id) {
 }
 
 echo(json_encode([
-    'message' => "Subscribed",
+    'message' => "Added to List",
     'code' => 200,
 ]));
 die;
